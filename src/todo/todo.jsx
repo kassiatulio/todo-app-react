02d3ed6195f8a3ee27fcf1a9ff2b6c31e6ada964
kassiatulio <kassia.tulio@gmail.com>
2019-07-09 11:58:27 +0100

    Conclusão do projeto e preparação para migração/implementação de redux

--- src/todo/todo.jsx
+++ src/todo/todo.jsx
@@ -19,6 +19,7 @@ export default class Todo extends Component {
         this.handleMarkAsPending = this.handleMarkAsPending.bind(this)
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this)
         this.handleSearch = this.handleSearch.bind(this)
+        this.handleClear = this.handleClear.bind(this)
 
         this.refresh()
     }
@@ -58,6 +59,10 @@ export default class Todo extends Component {
             .then(resp => this.refresh(this.state.description))
     }
 
+    handleClear(todo) {
+        this.refresh()
+    }
+
     render() {
         return (
             <div>
@@ -66,7 +71,8 @@ export default class Todo extends Component {
                     description={this.state.description}
                     handleChange={this.handleChange}
                     handleAdd={this.handleAdd} 
-                    handleSearch={this.handleSearch} />
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear} />
                 <TodoList 
                     list={this.state.list} 
                     handleMarkAsDone={this.handleMarkAsDone}
